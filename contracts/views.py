71b3a65725f7e90c800fc503abe00c7d585fff12
rodrigondec <rodrigondec@gmail.com>
2019-06-25 20:49:33 -0300

    contracts viewset

--- contracts/views.py
+++ contracts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from contracts.serializers import ContractSerializer, Contract
+
+
+class ContractViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows contracts to be viewed or edited.
+    """
+    queryset = Contract.objects.all()
+    serializer_class = ContractSerializer
+    authentication_classes = []
+    permission_classes = []
