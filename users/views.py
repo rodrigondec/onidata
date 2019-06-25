bb0d3ae28c9e9f6624c2ea3fab002d73b79f94e0
rodrigondec <rodrigondec@gmail.com>
2019-06-25 19:47:48 -0300

    user viewset actions with distinct serializers

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 
-from users.serializers import UserSerializer, User
+from users.serializers import (
+    UserCreateSerializer, UserListSerializer, UserUpdateSerializer,
+    User)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -8,6 +10,18 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserListSerializer
     authentication_classes = []
     permission_classes = []
+
+    def get_serializer_class(self):
+        update_actions = (
+            'update',
+            'partial_update',
+        )
+
+        if self.action == 'create':
+            return UserCreateSerializer
+        elif self.action in update_actions:
+            return UserUpdateSerializer
+        return self.serializer_class
