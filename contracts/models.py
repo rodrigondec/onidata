d73a11703547e5b18b3c8afa01af41813344a030
rodrigondec <rodrigondec@gmail.com>
2019-06-25 20:49:15 -0300

    contract Model + factory

--- contracts/models.py
+++ contracts/models.py
@@ -4,8 +4,8 @@ from core.models import BaseModel
 
 
 class Contract(BaseModel):
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    info = models.TextField()
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, help_text='Usuário do contrato')
+    info = models.TextField(help_text='Informações sobre o contrato')
 
     class Meta:
         ordering = ['id']
