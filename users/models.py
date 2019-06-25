cb944d3471ed4c95c4bb00cd0cbbfb80ce9e5b6f
rodrigondec <rodrigondec@gmail.com>
2019-06-25 20:07:59 -0300

    email unique

--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 from core.models import BaseModel
 
 
 class User(AbstractUser, BaseModel):
-    pass
+    email = models.EmailField(_('email address'), unique=True)
