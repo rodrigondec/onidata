734dc77bd676e56da0537268fbbd7350b1681739
rodrigondec <rodrigondec@gmail.com>
2019-06-25 19:47:30 -0300

    Specific serializers for users

--- users/serializers.py
+++ users/serializers.py
@@ -3,10 +3,37 @@ from rest_framework import serializers
 from users.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email']
+        fields = ['id', 'email', 'password', 'username', 'first_name', 'last_name']
+        extra_kwargs = {
+            'id': {'read_only': True},
+        }
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'first_name', 'last_name']
+        extra_kwargs = {
+            'id': {'read_only': True},
+        }
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name']
+        extra_kwargs = {
+            'id': {'read_only': True},
+        }
+
+
+class PasswordResetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'password']
         extra_kwargs = {
             'id': {'read_only': True},
         }
