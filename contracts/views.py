500ba732154412382c3f8c0db8561ad3cbad7a08
rodrigondec <rodrigondec@gmail.com>
2019-06-26 17:38:22 -0300

    contract & payment read viewsets

--- contracts/views.py
+++ contracts/views.py
@@ -1,9 +1,21 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework_extensions.mixins import NestedViewSetMixin
 
 from contracts.serializers import ContractSerializer, Contract
 
 
-class ContractViewSet(viewsets.ModelViewSet):
+class ContractReadViewSet(NestedViewSetMixin, GenericViewSet, ListModelMixin, RetrieveModelMixin):
+    """
+    API endpoint that allows contracts to be viewed.
+    """
+    queryset = Contract.objects.all()
+    serializer_class = ContractSerializer
+    authentication_classes = []
+    permission_classes = []
+
+
+class ContractViewSet(ModelViewSet):
     """
     API endpoint that allows contracts to be viewed or edited.
     """
