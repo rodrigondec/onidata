111bfdd0da660e1e69c64239ffa51ac8ec42726f
rodrigondec <rodrigondec@gmail.com>
2019-06-26 17:43:31 -0300

    value on contract

--- contracts/models.py
+++ contracts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 from core.models import BaseModel
 
@@ -6,6 +7,11 @@ from core.models import BaseModel
 class Contract(BaseModel):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, help_text='Usuário do contrato')
     info = models.TextField(help_text='Informações sobre o contrato')
+    value = models.DecimalField(
+        decimal_places=2,
+        max_digits=1000,
+        help_text='Valor do pagamento',
+        validators=[MinValueValidator(limit_value=1)])
 
     class Meta:
         ordering = ['user_id']
