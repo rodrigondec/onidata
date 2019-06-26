63603cda26b6f60fd9b124b56934b2e7d77d1786
rodrigondec <rodrigondec@gmail.com>
2019-06-25 22:04:27 -0300

    payments viewset + urls

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from payments.serializers import PaymentSerializer, Payment
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows payments to be viewed or edited.
+    """
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    authentication_classes = []
+    permission_classes = []
