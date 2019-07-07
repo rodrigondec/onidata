488fb5c5c57fc80519adc50d052b0235b4dd2271
rodrigondec <rodrigondec@gmail.com>
2019-07-07 01:09:39 -0300

    contracts tests ok

--- contracts/tests/test_models.py
+++ contracts/tests/test_models.py
@@ -11,12 +11,12 @@ class ContractTestCase(TestCase):
         self.assertEqual(Contract.objects.count(), 1)
 
     def test_update(self):
-        contract = ContractFactory(info='Informação antiga')
-        self.assertEqual(Contract.objects.first().info, 'Informação antiga')
+        contract = ContractFactory(bank='Informação antiga')
+        self.assertEqual(Contract.objects.first().bank, 'Informação antiga')
 
-        contract.info = 'Nova informação'
+        contract.bank = 'Nova informação'
         contract.save()
-        self.assertEqual(Contract.objects.first().info, 'Nova informação')
+        self.assertEqual(Contract.objects.first().bank, 'Nova informação')
 
     def test_delete(self):
         contract = ContractFactory()
@@ -26,7 +26,7 @@ class ContractTestCase(TestCase):
         self.assertEqual(Contract.objects.count(), 0)
 
     def test_amount_due(self):
-        contract = ContractFactory(initial_value=150, interest_rate=0)
+        contract = ContractFactory(amount=150, interest_rate=0)
         self.assertIsInstance(contract, Contract)
 
         PaymentFactory.create_batch(2, contract=contract, value=10)
@@ -35,7 +35,7 @@ class ContractTestCase(TestCase):
 
     @freeze_time('2019-05-05 12:00:00')
     def test_updated_value(self):
-        contract = ContractFactory(initial_value=150, interest_rate=1, start_date='2019-04-01')
+        contract = ContractFactory(amount=150, interest_rate=1, submission_date='2019-04-01')
         self.assertIsInstance(contract, Contract)
 
         self.assertEqual(contract.updated_value, 300)
