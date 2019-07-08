5f5b71d9df01cc1d5fa3799bfe7efc8d07ea26d4
rodrigondec <rodrigondec@gmail.com>
2019-07-07 21:58:55 -0300

    better permission return

--- core/permissions.py
+++ core/permissions.py
@@ -3,9 +3,8 @@ from rest_framework import permissions
 
 class IsAuthenticatedOrCreate(permissions.IsAuthenticated):
     def has_permission(self, request, view):
-        if request.method == 'POST':
-            return True
-        return super(IsAuthenticatedOrCreate, self).has_permission(request, view)
+        return (super().has_permission(request, view) or
+                request.method == 'POST')
 
 
 class IsOwnerOrCreate(permissions.BasePermission):
